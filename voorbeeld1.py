4ba060a360fc1148421d87cd40b352efc5a2fd31
Jan-Willem Roode <j.roode@aventus.nl>
2022-03-11 13:16:48 +0100

    nog een vb

--- voorbeeld1.py
+++ voorbeeld1.py
@@ -5,7 +5,7 @@ window = tk.Tk()
 frame_a = tk.Frame()
 frame_b = tk.Frame()
 
-label_a = tk.Label(master=frame_a, text="I'm in Frame A")
+label_a = tk.Label(master=frame_a, text="I'm in Frame A en nu aanpassen")
 label_a.pack()
 
 label_b = tk.Label(master=frame_b, text="I'm in Frame B")
